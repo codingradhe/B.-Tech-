6d16b5a08cb67857c5b3e5636b25d5a463013a34
Radheshyam chaudhary <rc842944@gmail.com>
2026-08-05 01:07:52 +0530

    Add merge sort implementation in 11.mergeSort.c
    Implement merge sort algorithm with merge function.

---
+++ Sem3/CDSA/Unit-1/11.mergeSort.c
@@ -0,0 +1,54 @@
+#include <stdio.h>
+void merge(int a[],int m,int b[],int n,int ans[],int size){
+    int i =0, j = 0,k = 0;
+    while(i < m&& j<n){
+        if(a[i] <= b [j]){
+            ans[k]= a[i];
+            i++,k++;
+        }
+        else {
+            ans[k] = b[j];
+            j++,k++;
+        }
+    }
+    while(i < m){
+        ans[k] = a[i];
+        i++,k++;
+    }
+    while(j < n){
+        ans[k] = b[j];
+        j++,k++;
+    }
+    return;
+}
+void mergeSort(int arr[],int n){
+    int size = (sizeof(arr))/sizeof(arr[0]);
+    if((size<2) return;
+    int mid = size/2;
+    int a[mid],b[size-mid];
+    for(int i =0;i<size;i++){
+        if(i<mid) a[i] = arr[i];
+        else b[i-mid]   =  arr[i];
+    }
+    mergeSort(a,mid);
+    mergeSort(b,size-mid);
+    merge(a,mid,b,size-mid,arr,size);
+    return;
+}
+    
+int main() {
+  int n;
+  printf("Enter size of array ::\n ");
+  scanf("%d%" ,&n);
+  int arr[n];
+  printf("Enter elements of  array::\n ");
+  for(int i=0;i<n;i++){
+    scanf("%d",&arr[i]);
+  }
+  mergeSort(arr,n);
+  printf("Your sorted array :: \n");
+  for(int i=0;i<n;i++){
+    printf("%d ",ans[i]);
+  }
+  return 0;
+}
